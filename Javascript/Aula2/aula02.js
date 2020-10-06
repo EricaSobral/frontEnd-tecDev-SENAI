d4faeebb2c1d2fc72e69ecfaa761aea5c3880658
EricaSobral <erica.sobral95@gmail.com>
2020-10-06 11:26:02 -0200

    Enviando exercicios de JavaScript/React - SENAI

---
+++ Javascript/Aula2/aula02.js
@@ -0,0 +1,19 @@
+
+// Exercício 1 - Está chovendo 
+// var chuva = true
+
+// if (chuva == true) {
+//     console.log("Está chovendo")
+// }else{
+//     console.log("Não está chovendo")
+// }
+
+//Exercício 2 - Bebidas alcoólicas para maiores 
+var idade = 18
+
+
+if (idade < 18){
+    console.log("Menores de 18 anos não podem consumir bebidas alcoólicas")
+}else{
+    console.log("Maiores de idade podem comprar bebidas alcoólicas")
+}
